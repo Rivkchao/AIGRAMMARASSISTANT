d40fd8339796612492e581d27ce1422398f1d5cf
Rivky Putra Setiawan <115005666+Rivkchao@users.noreply.github.com>
2025-11-24 13:20:58 +0000

    apt

--- tense_detection.py
+++ tense_detection.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-OPENROUTER_API_KEY = st.secrets("OPENAI_API_KEY")
+OPENROUTER_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 def detect_tense_openai(text):
     """
